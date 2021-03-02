9eaff88074620eb589a619aa3edc736215751790
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-02 02:46:01 -0300

    Criando formulário da páfina de orçamento

--- pages/orcamento.js
+++ pages/orcamento.js
@@ -1,7 +1,7 @@
 import Menu from '../components/Menu';
 import Footer from '../components/Footer';
 
-import { Container, Jumbotron} from 'reactstrap';
+import {  Container, Jumbotron, Button, Form, FormGroup, Label, Input} from 'reactstrap';
 
 function orcamento(){
     return(
@@ -25,9 +25,46 @@ function orcamento(){
 
         </Jumbotron>
 
+        <Jumbotron fluid className="form-orcamento">
+        <style>
+          {`.form-orcamento{
+              padding-top: 80px;
+              padding-bottom: 80px;
+              background-color: #fff;
+              margin-bottom: 0rem !important;
+            }`}
+        </style>
         <Container>
-           <h1>Página de orçamento</h1>
+          <Form>
+            <FormGroup>
+              <Label for="name">Nome</Label>
+              <Input type="text" name="name" id="name" placeholder="Prencha com o nome completo" />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="email">E-mail</Label>
+              <Input type="email" name="email" id="email" placeholder="Prencha com o seu melhor e-mail" />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="phone">Telefone</Label>
+              <Input type="text" name="phone" id="phone" placeholder="(XX) XXXX-XXXX" />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="whatsApp">WhatsApp</Label>
+              <Input type="text" name="whatsApp" id="whatsApp" placeholder="(XX) XXXX-XXXX" />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="projeto">Projeto</Label>
+              <Input type="textarea" name="projeto" id="projeto" placeholder="Fale um pouco do seu projeto" />
+            </FormGroup>
+
+            <Button type="submit" outline color="primary">Solicitar</Button>
+          </Form>
         </Container>
+      </Jumbotron>
          
         <Footer />  
         </div>
