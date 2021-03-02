77c8f428c640cab60bb600cf906a79fe9274e66f
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-02 01:00:22 -0300

    Configurando menu com Bootstrap

--- components/Menu.js
+++ components/Menu.js
@@ -1,8 +1,38 @@
-function Menu(){
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+  } from 'reactstrap';
+
+const Menu = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
     return(
         <div>
-            <a href='/'>Home</a><br/>
-            <a href='/orcamento'>Orçamento</a>
+            <Navbar color="primary" dark expand="md">
+                <Container>
+                    <NavbarBrand href="/">Orçamento</NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                        <NavLink href="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                        <NavLink href="/orcamento">Orçamento</NavLink>
+                        </NavItem>
+                        
+                    </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 }
