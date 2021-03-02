00ebe03780050df7daf60f4e545d6405bb5296ed
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-02 01:15:49 -0300

    Trabalhando com Jumbotron

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Jumbotron, Button, Container } from 'reactstrap';
 import Menu from '../components/Menu';
 
 function Home(){
@@ -7,7 +7,22 @@ function Home(){
     return(
         <div>
            <Menu />
-           <Button color="warning">Enviar</Button>
+           <Jumbotron fluid className="descr-top">
+                <style>
+                    {`.descr-top{
+                        background-color: #050c3d;
+                        color: #00a1fc;
+                        margin-bottom: 0rem !important;
+                    }`}
+                </style>
+               <Container className="text-center">
+               <h1 className="display-4">Temos a solução que a sua empresa precisa!</h1>
+                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <p className="lead">
+                        <a href='/orcamento' className="btn btn-outline-primary btn-lg mt-4">Orçamento</a>
+                    </p>
+                </Container>
+      </Jumbotron>
         </div>
     ); 
 }
