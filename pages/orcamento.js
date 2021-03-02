f6858256a7f929d71b742929482411e4fe3e431f
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-02 02:08:12 -0300

    Estrutura da página de orçamento

--- pages/orcamento.js
+++ pages/orcamento.js
@@ -1,9 +1,35 @@
 import Menu from '../components/Menu';
+import Footer from '../components/Footer';
+
+import { Container, Jumbotron} from 'reactstrap';
 
 function orcamento(){
     return(
         <div>
           <Menu />
+
+          <Jumbotron fluid className="descr-top">
+        <style>
+          {`.descr-top{
+              background-color: #050c3d;
+              color: #00a1fc;
+              padding-top: 40px;
+              padding-bottom: 40px;
+              margin-bottom: 0rem !important;
+          }`}
+        </style>
+
+        <Container className="text-center">
+          <h1 className="display-4">Orçamento</h1>
+        </Container>
+
+        </Jumbotron>
+
+        <Container>
+           <h1>Página de orçamento</h1>
+        </Container>
+         
+        <Footer />  
         </div>
     ); 
 }
