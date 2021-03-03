5a88ebb2f7e7c1fa50833d147c121ce14cf5149d
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-03 00:01:22 -0300

    Criando primeira rota

--- api/app.js
+++ api/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, function(){
-    console.log("Servidor iniciado na porta 8080")
+app.get('/home', function (req, res){
+    res.send('hello world');
+});
+
+app.listen(8080, function(){
+    console.log("Servidor iniciado na porta 8080");
 });
