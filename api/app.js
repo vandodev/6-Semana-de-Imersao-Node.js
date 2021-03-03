d7d33c91e02578f9477d5b0f5a2f5c8326b824c6
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-03 00:31:04 -0300

    instalando dependências e conectando com mongoDb

--- api/app.js
+++ api/app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/celke6', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log("Conexão com o BD MongoDB realizado com sucesso!");
+}).catch((err) => {
+    console.log("Erro: Conexão com o BD MongoDB não realizado!");
+})
 
 const app = express();
 
