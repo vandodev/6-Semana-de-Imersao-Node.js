e20703f3e42c2cac11bb88426a44fe579e916403
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-03 02:17:28 -0300

    Trabalhando com cors

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 require('./models/home');
 const Home = mongoose.model('Home');
@@ -7,15 +8,25 @@ const Home = mongoose.model('Home');
 require('./models/orcamento');
 const Orcamento = mongoose.model('Orcamento');
 
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+});
+
+
 mongoose.connect('mongodb://localhost:27017/celke6', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log("Conexão com o BD MongoDB realizado com sucesso!");
 }).catch((err) => {
     console.log("Erro: Conexão com o BD MongoDB não realizado!");
 })
 
-const app = express();
-
-app.use(express.json());
 
 app.get('/home', async (req, res) => {
     await Home.findOne({}).then((home) => {
