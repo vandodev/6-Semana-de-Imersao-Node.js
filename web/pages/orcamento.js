a6208a895e8a836ef83f8abca5d4e2d7da7bc472
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-04 01:01:28 -0300

    Conectando front com back, página de orçamento

--- web/pages/orcamento.js
+++ web/pages/orcamento.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import Head from 'next/head';
 import Menu from '../components/Menu';
 import Footer from '../components/Footer';
@@ -6,6 +8,43 @@ import {  Container, Jumbotron, Button, Form, FormGroup, Label, Input} from 'rea
 
 
 function orcamento(){
+
+  const [orcamento, setOrcamento] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    whatsApp: '',
+    projeto: ''
+  });
+
+  const onChangeInput = e => setOrcamento({ ...orcamento, [e.target.name]: e.target.value });
+
+  const sendOrcamento = async e => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('http://localhost:8080/orcamento', {
+        method: 'POST',
+        body: JSON.stringify(orcamento),
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+      const responseEnv = await res.json();
+
+      if (responseEnv.error) {
+         alert(responseEnv.message);
+      } else {
+        alert(responseEnv.message);
+      }
+    } catch (err) {
+      setRespose({
+        formSave: false,
+        type: 'error',
+        message: "Erro: Orçamento não enviado com sucesso!"
+      });
+    }
+  }
+
     return(
         <div>
 
@@ -43,30 +82,30 @@ function orcamento(){
             }`}
         </style>
         <Container>
-          <Form>
+          <Form onSubmit={sendOrcamento}>
             <FormGroup>
               <Label for="name">Nome</Label>
-              <Input type="text" name="name" id="name" placeholder="Prencha com o nome completo" />
+              <Input type="text" name="name" id="name" placeholder="Prencha com o nome completo" onChange={onChangeInput} />
             </FormGroup>
 
             <FormGroup>
               <Label for="email">E-mail</Label>
-              <Input type="email" name="email" id="email" placeholder="Prencha com o seu melhor e-mail" />
+              <Input type="email" name="email" id="email" placeholder="Prencha com o seu melhor e-mail" onChange={onChangeInput}/>
             </FormGroup>
 
             <FormGroup>
               <Label for="phone">Telefone</Label>
-              <Input type="text" name="phone" id="phone" placeholder="(XX) XXXX-XXXX" />
+              <Input type="text" name="phone" id="phone" placeholder="(XX) XXXX-XXXX" onChange={onChangeInput} />
             </FormGroup>
 
             <FormGroup>
               <Label for="whatsApp">WhatsApp</Label>
-              <Input type="text" name="whatsApp" id="whatsApp" placeholder="(XX) XXXX-XXXX" />
+              <Input type="text" name="whatsApp" id="whatsApp" placeholder="(XX) XXXX-XXXX" onChange={onChangeInput}/>
             </FormGroup>
 
             <FormGroup>
               <Label for="projeto">Projeto</Label>
-              <Input type="textarea" name="projeto" id="projeto" placeholder="Fale um pouco do seu projeto" />
+              <Input type="textarea" name="projeto" id="projeto" placeholder="Fale um pouco do seu projeto" onChange={onChangeInput}/>
             </FormGroup>
 
             <Button type="submit" outline color="primary">Solicitar</Button>
