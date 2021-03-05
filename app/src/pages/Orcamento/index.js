49e615e702c3350f0cdcd5491069b83446747a9b
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-05 01:52:43 -0300

    Conectando aplicativo com api

--- app/src/pages/Orcamento/index.js
+++ app/src/pages/Orcamento/index.js
@@ -1,38 +1,91 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React , { useState }  from 'react';
+import {Alert,  ActivityIndicator} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
-import {useNavigation} from '@react-navigation/native';
 
-import {Container, TitleInput, InputForm, BtnSubmitForm, TxtSubmitForm} from './styles'
+import {Container, TitleInput, InputForm, BtnSubmitForm, TxtSubmitForm, LoadingArea} from './styles';
+
+import api from '../../config/api';
 
 export default function Orcamento() {
-  const navigation = useNavigation();
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [whatsApp, setWhatsApp] = useState('');
+    const [projeto, setProjeto] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const addOrcamento = async () => {
+      setLoading(true);
+      await api.post('/orcamento', { name, email, phone, whatsApp, projeto }).then((response) => {
+          Alert.alert("", response.data.message);
+          setLoading(false);
+      }).catch((err) => {
+          if (err.response) {
+              Alert.alert("", err.response.data.message);
+              setLoading(false);
+          } else {
+              Alert.alert("", "Erro: Orçamento não enviado, tente mais tarde!");
+              setLoading(false);
+          };
+      });
+  };
+
 
   return (
     <ScrollView contentContainerStyle={{flexGrow:1}}>
       <Container>
 
         <TitleInput>Nome</TitleInput>
-        <InputForm placeholder="Nome completo" autoCorrect={false}/>
+        <InputForm
+                    placeholder="Nome completo"
+                    autoCorrect={false}
+                    value={name}
+                    onChangeText={text => setName(text)} />
 
         <TitleInput>Email</TitleInput>
-        <InputForm placeholder="Digite seu email" autoCorrect={false}/>
+        <InputForm
+                    placeholder="Seu melhor e-mail"
+                    autoCorrect={false}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    value={email}
+                    onChangeText={text => setEmail(text)} />
+
 
         <TitleInput>Telefone</TitleInput>
-        <InputForm placeholder="(xx)xxxxx-xxxx" autoCorrect={false}/>
+        <InputForm 
+                    placeholder="(XX) XXXX-XXXX"
+                    autoCorrect={false}
+                    value={phone}
+                    onChangeText={text => setPhone(text)} />
 
         <TitleInput>WatsApp</TitleInput>
-        <InputForm placeholder="(xx)xxxxx-xxxx" autoCorrect={false}/>
+        <InputForm
+                    placeholder="(XX) XXXX-XXXX"
+                    autoCorrect={false}
+                    value={whatsApp}
+                    onChangeText={text => setWhatsApp(text)} />
 
         <TitleInput>Projeto</TitleInput>
-        <InputForm placeholder="Fale sobre o projeto" autoCorrect={false}/>
+        <InputForm
+                    placeholder="Fale um pouco do seu projeto"
+                    autoCorrect={false}
+                    value={projeto}
+                    onChangeText={text => setProjeto(text)} />
 
-        <BtnSubmitForm>
+        <BtnSubmitForm disabled={loading} onPress={addOrcamento}>
           <TxtSubmitForm>
             Cadastrar
           </TxtSubmitForm>
         </BtnSubmitForm>
         
+        {loading &&
+                    <LoadingArea>
+                        <ActivityIndicator size="large" color="#fff" />
+                    </LoadingArea>
+        }
+
       </Container>
     </ScrollView>
     
